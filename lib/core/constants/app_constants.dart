a7370ad44e1764c2baa9d958e787e73bbc89a2f5
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-06-16 19:36:12 +0300

    Update native platforms config metadata and AppConstants fallbacks for Rania

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,28 +4,28 @@ class AppConstants {
   static Map<String, dynamic> get _info =>
       PortfolioData.data['personalInfo'] ?? {};
 
-  static String get devName => _info['name'] ?? 'Mohamed Refky';
-  static String get devTitle => _info['title'] ?? 'Flutter Developer';
+  static String get devName => _info['name'] ?? 'Rania Abdelbary';
+  static String get devTitle => _info['title'] ?? 'Architectural Engineer | BIM Specialist';
   static String get devDescription =>
       _info['description'] ??
-      'I am a passionate Flutter developer with a strong foundation in mobile app development. I enjoy building high-quality, user-friendly applications that solve real-world problems. With experience in both frontend and backend development, I am committed to creating seamless and efficient mobile experiences.';
+      'BIM-focused Architectural Engineer with 8+ years of experience in architectural design, site supervision, and finishing works management.';
 
   static String get devImagePath =>
-      _info['image'] ?? 'assets/images/my_image.jpg';
+      _info['image'] ?? 'assets/images/rania_image.png';
   static String get devCvPath =>
       _info['cv'] ??
-      'https://drive.google.com/file/d/1o4O1oOJMVoQpqk9DghUuFOFuRJt7Oox4/view?usp=drive_link';
+      '';
 
   // Social Links
   static String get githubUrl =>
-      _info['github'] ?? 'https://github.com/MohamedRefky';
+      _info['github'] ?? '';
   static String get linkedinUrl =>
-      _info['linkedin'] ?? 'https://www.linkedin.com/in/mohamedrefky/';
+      _info['linkedin'] ?? 'https://www.linkedin.com/in/rania-abdelbary-718b85342';
   static String get email =>
-      'mailto:${_info['email'] ?? 'mohamedrifky9765@gmail.com'}';
+      'mailto:${_info['email'] ?? 'ranianasr609@gmail.com'}';
   static String get whatsappNumber =>
-      _info['whatsappNumber'] ?? '+201019964918';
+      _info['whatsappNumber'] ?? '+201011712614';
   static String get whatsappUrl =>
       'https://wa.me/${whatsappNumber.replaceAll('+', '')}';
-  static String get phoneCallUrl => 'tel:${_info['phone'] ?? '+201019964918'}';
+  static String get phoneCallUrl => 'tel:${_info['phone'] ?? '+971501587005'}';
 }
