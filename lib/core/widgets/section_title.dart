5f21d7e42f6d873a0e56f909cb28b3cf02322162
Mohamed Refky <mohamedrifky9765@gmail.com>
2026-06-17 16:32:22 +0300

    feat: add reusable SectionTitle widget with entrance animations

--- lib/core/widgets/section_title.dart
+++ lib/core/widgets/section_title.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import '../theme/app_colors.dart';
+
 
 class SectionTitle extends StatelessWidget {
   final String title;
